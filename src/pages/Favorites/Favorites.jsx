ebc9dc04676a1397db2dc92cb1e961bc61f38c3e
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-07-24 11:13:22 +0300

    feat: add Home, Teachers and Favorites page stubs

---
+++ src/pages/Favorites/Favorites.jsx
@@ -0,0 +1,13 @@
+import Container from '../../components/Container/Container';
+
+function Favorites() {
+  return (
+    <section>
+      <Container>
+        <h1>Favorites</h1>
+      </Container>
+    </section>
+  );
+}
+
+export default Favorites;
