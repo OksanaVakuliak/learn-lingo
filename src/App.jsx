764c98df6ea73fc10a624b537607c78ba5442ba4
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-07-24 11:17:17 +0300

    feat: configure application routes and router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Home from './pages/Home/Home';
+import Teachers from './pages/Teachers/Teachers';
+import Favorites from './pages/Favorites/Favorites';
+
 function App() {
-  return <h1>LearnLingo</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="teachers" element={<Teachers />} />
+        <Route
+          path="favorites"
+          element={
+            <PrivateRoute>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
